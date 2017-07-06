8f7d92e992ca0a78c887d710226de99cc1332875
André Ferreira <contato@andreferreira.me>
2017-07-06 12:23:51 -0300

    feat: Login, Logout and validation

--- models/user.js
+++ models/user.js
@@ -30,3 +30,23 @@ module.exports.createUser = (newUser, callback) => {
     });
   });
 }
+
+module.exports.getUserById = (id, callback) => {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+  const query = {
+    username
+  };
+  User.findOne(query, callback);
+}
+
+module.exports.comparePassword = (candidatePassword, hash, callback) => {
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if (err) 
+      throw err;
+    
+    callback(null, isMatch);
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
 
@@ -24,29 +26,35 @@ router.post('/register', (req, res) => {
   const password2 = req.body.password2;
 
   // Validation
-  req.checkBody('name', 'Name is required').notEmpty();
-  req.checkBody('email', 'Email is rquired').notEmpty();
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('username', 'Username is required').notEmpty();
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+  req
+    .checkBody('name', 'Name is required')
+    .notEmpty();
+  req
+    .checkBody('email', 'Email is rquired')
+    .notEmpty();
+  req
+    .checkBody('email', 'Email is not valid')
+    .isEmail();
+  req
+    .checkBody('username', 'Username is required')
+    .notEmpty();
+  req
+    .checkBody('password', 'Password is required')
+    .notEmpty();
+  req
+    .checkBody('password2', 'Passwords do not match')
+    .equals(req.body.password);
 
   const errors = req.validationErrors();
 
-  if (errors){
-    res.render('register', {
-      errors: errors
-    });
-  }else{
-    const newUser = new User({
-      name,
-      email,
-      username,
-      password
-    });
+  if (errors) {
+    res.render('register', {errors: errors});
+  } else {
+    const newUser = new User({name, email, username, password});
 
     User.createUser(newUser, (err, user) => {
-      if (err) throw err;
+      if (err) 
+        throw err;
       console.log(user);
     });
 
@@ -54,6 +62,53 @@ router.post('/register', (req, res) => {
 
     res.redirect('/users/login');
   }
+});
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.getUserByUsername(username, (err, user) => {
+    if (err) 
+      throw err;
+
+    if (!user) {
+      return done(null, false, {message: 'Unknown User'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if (err) 
+        throw err;
+
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, {message: 'Invalid password'});
+      }
+
+    });
+  });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.getUserById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login',
+  failureFlash: true
+}), (req, res) => {
+  res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
 });
 
 module.exports = router;
